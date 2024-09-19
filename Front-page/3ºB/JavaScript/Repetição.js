a3c4b9251dca3115afd5063de50e1f3855aee344
Dedei-AB <andreiabners@gmail.com>
2024-09-19 10:18:53 -0300

    Tabuada

---
+++ Front-page/3ºB/JavaScript/Repetição.js
@@ -0,0 +1,9 @@
+function tabuada(n){
+    let cont = 1
+    while(cont<=10){
+        console.log(`${n}x${cont} = ${n*cont}`)
+        cont ++
+    }
+}
+//Coloque a tabuada que vc quer no parênteses abaixo
+tabuada(5)
