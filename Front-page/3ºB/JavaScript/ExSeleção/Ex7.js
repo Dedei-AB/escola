22ae73be347a1626bb153e181cdec0ec640f78d0
Dedei-AB <andreiabners@gmail.com>
2024-09-30 11:32:46 -0300

    return do ex 7

--- Front-page/3ºB/JavaScript/ExSeleção/Ex7.js
+++ Front-page/3ºB/JavaScript/ExSeleção/Ex7.js
@@ -1,8 +1,10 @@
 function carro(valorGasolina, valorAlcool){
     if (valorGasolina * 0.7 >= valorAlcool){
         console.log('Ainda compensa usar álcool.')
+        return true
     }else{
         console.log('Não compensa usar álcool.')
+        return false
     }
 }
-carro(6.09, 2.77);
+carro(6.09, 3);
