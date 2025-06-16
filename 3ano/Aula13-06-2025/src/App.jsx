ec9a811d94c9184dfa9824a67402097b9b9a1f9b
Dedei-AB <andreiabners@gmail.com>
2025-06-16 07:44:18 -0300

    ultima aula

---
+++ 3ano/Aula13-06-2025/src/App.jsx
@@ -0,0 +1,13 @@
+import TarefaInput from "./TarefaInput";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Gerenciador de Tarefas</h1>
+      <TarefaInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ 3ano/Aula13-06-2025/src/TarefaItem.jsx
@@ -0,0 +1,19 @@
+export default function TarefaItem({
+  tarefa,
+  onToggleConcluir,
+  onRemoverTarefa,
+}) {
+  return (
+    <li>
+      <span
+        style={{ textDecoration: tarefa.concluida ? "line-throug" : "none" }}
+      >
+        {tarefa.descricao}
+      </span>
+      <button onClick={() => onToggleConcluir(tarefa.id)}>
+        {tarefa.concluida ? "Desfazer" : "Concluir"}
+      </button>
+      <button onClick={() => onRemoverTarefa(tarefa.id)}>Remover</button>
+    </li>
+  );
+}
